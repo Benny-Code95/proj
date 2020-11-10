14149606505cd776d00867ee50b7cf183f449e7d
Benny-Code95 <1287343797@qq.com>
2020-11-10 16:49:08 +0800

    测试

--- labs/lab.py
+++ labs/lab.py
@@ -1,18 +1,6 @@
-import re
-
-
-def parseMaterialTXT(txtPath):
-    with open(txtPath, 'r', encoding='utf8') as f:
-        data = f.read()
-    print(data)
-    cmp = re.compile(
-        'W=\".(\d+\.?\d+)\" n=\".(\d*\.\d*)\" k=\".(\d*\.?\d*(E-\d+)?)')
-    nks = re.findall(cmp, data)
-    for nk in nks:
-        print('{0}\t{1}\t{2}'.format(float(nk[0]), float(nk[1]), float(nk[2])))
+def tst():
+    pass
 
 
 if __name__ == '__main__':
-    path = 'F:\\研究生\\计算及工作整理\\截止膜\\TFC设计\\sio2.txt'
-    parseMaterialTXT(path)
     pass
