03d2def5528bf899ca1097046ab3086076db2bec
Benny-Code95 <1287343797@qq.com>
2020-11-13 10:32:11 +0800

    test

---
+++ labs/gitLab.py
@@ -0,0 +1,21 @@
+# coding utf-8
+import os
+
+
+def CeShiFunc():
+    os.getcwd()
+    return 
+
+
+class CeShiCls(object):
+
+    def __init__(self):
+        self.a = 0
+
+    def cs(self):
+        pass
+
+
+if __name__ == '__main__':
+    CeShiFunc()
+    print(CeShiCls().a)
